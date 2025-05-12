d8b43b84fea8d78735aa3b83170779de0d27fe2d
Andreiadsm <martins.andreiad@gmail.com>
2025-05-12 13:30:11 +0100

    aula jvscript

--- js/exs.js
+++ js/exs.js
@@ -26,31 +26,119 @@ console.log(facialHair)
 
 //ex2 pp38
 
-let password = prompt("Indique a sua password") 
-console.log(password)
-if (password.length > 5){
-    alert ("acesso permitido");
-} else {"Insira 6 caracteres"};
+// let password = prompt("Indique a sua password") 
+
+// if (password.length < 6){
+//     console.log ("insira 6 caracteres");
+// } else{
+//     console.log ("pass ok");
+// }
+
+// //ex3 pp 38 - toLowerCase e trim tb funciona no prompt
+// let weekDay = prompt ("que dia da semana é");
+
+// if (weekDay.toLowerCase().trim() == "Sexta"){
+//     console.log("yay sobrevivemos a mais uma semana");
+// }else{
+//     console.log("força nisso");
+// }
 
 
-//ex3 pp 38
-let weekDay = prompt("Hoje que dia da semana é?")
-switch (weekDay) {
-    case "Segunda":
-        alert ("Segunda");
-        break;
-    case "Terça":
-        alert("Terça");
-        break;
-    case "Quarta":
-        alert("Quarta");
-        break;
-    case "Quinta":
-        alert ("Quinta");
-        break;
-    case "Sexta":
-        alert ("Yay sobrevivemos a mais uma semana");
-        break;
-    default:
-        alert("ups, não temos essa opção")
+// //ex3 pp 38
+// let weekDay = prompt("Hoje que dia da semana é?")
+// switch (weekDay) {
+//     case "Segunda":
+//         alert ("Segunda");
+//         break;
+//     case "Terça":
+//         alert("Terça");
+//         break;
+//     case "Quarta":
+//         alert("Quarta");
+//         break;
+//     case "Quinta":
+//         alert ("Quinta");
+//         break;
+//     case "Sexta":
+//         alert ("Yay sobrevivemos a mais uma semana");
+//         break;
+//     default:
+//         alert("ups, não temos essa opção")
+// }
+
+//ex58 pag 43 arrays
+
+// let planets = ["Mercúrio", "Vénus", "Terra", "Mart", "Jupiter", "Saturno", "Urano", "Neptuno", "Plutão"]
+// planets[3] = "Marte";
+// planets.pop();
+// planets.unshift("khay");
+// console.log(planets);
+
+//ex page 55
+
+// for (let i=0; i<6; i++) {
+//     console.log (i+"Da ba dee da ba daa");
+// }
+
+// let numeros = [25, 20, 15, 10, 5]
+// for (let i=0; i<1; i++) {
+//     console.log (numeros)
+// }
+
+// for(let i=25; i>=5; i-=5){
+//     console.log(i);
+// }
+
+//ex pag 58
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+for (let i=0; i < people.length; i++){
+    console.log(people[i].toUpperCase());
 }
+
+//ex pag 62
+
+// let shoppingList = []; // Lista de compras
+// let item; // Variável para armazenar o item digitado
+
+// while (true) {
+//     item = prompt("Adicione um item à lista (ou digite 'código' para finalizar):");
+
+//     // Verifica se o usuário digitou 'código' para sair
+//     if (item.toLowerCase() === "código") {
+//         break; // Sai do loop
+//     }
+
+//     // Adiciona o item à lista
+//     shoppingList.push(item);
+// }
+
+// // Mostra a lista final
+// console.log("A sua lista de compras:");
+// for (let produto of shoppingList) {
+//     console.log("- " + produto);
+// }
+
+//ou
+
+let shoppingList = [];
+let item="";
+
+while (item !== "código") {
+    item = prompt("Adicione um item à sua lista (ou digite 'código' para finalizar):");
+
+    if (item !== "código") {
+        shoppingList.push(item);
+    } else{console.log("A sua lista de compras:");
+    }
+}
+
+for (let item of shoppingList){
+    console.log(item)
+}
+
+if (item !== "fim") {
+    item= prompt("Digite 'fim' para terminar):");
+}
+else (item === "fim") {
+    console.log(shoppingList.trim)}
+
--- js/script.js
+++ js/script.js
@@ -9,4 +9,38 @@ console.log(`o meu nome é ${myName} e o meu número preferido é ${myLuckyNumbe
 
 let millionDollarQuestion = prompt('que tal a semana com feriado e apagão?'); //basicamente defino o prompt e guardo a resposta do user dentro da variável e os dados recebidos são strings
 
+//para que servem os arrays para nao termos de declarar variáveis de forma repetida tipo dias de semana, e outros grupos
+let day1 = "segunda";
+let day2= "terça";
+let day3 = "quarta" //and so one; mas isto não é viável então usamos um array
 
+
+//arrays
+
+let weekDays = ["segunda", "terça", "quarta"];
+let studentNotes = [ 18, 19, 20, 17];
+let ourSuperArr = ["segunda", true, "Joana", 17];
+
+console.log("o array na posição 1 tem" + weekDays[1].toUpperCase);
+
+//modificar arrays- pensar numa lista de compras . usamos metodos para colocar coisas em determinada posição
+//Push: adiciona ao fim
+//Pop: remove do fim;
+//Shift: remove do ínicio;
+//Unshift: adiciona ao inicio;
+
+//métodos de arrays
+
+let shoppingList = ["batatas", "feijão"];
+
+// let item = prompt("Qual é o item a adicionar");
+// shoppingList.push(item)
+// console.log(shoppingList)
+
+// for(let i=0; i>shoppingList.length; i++){
+//     console.log("compra" + shoppingList[i]);
+// }
+//podemos usar o método acima similar a java mas devemos simplificar e em javascript usar a variável declarada( aqui item). Para cada elemtno do array imprime elemento.
+for (let item of shoppingList){
+    console.log(item)
+}
